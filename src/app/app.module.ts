a0c11b3164c1b074bcd4fe3d70e706cea5a7dfa4
hsgoes <hugo.henkel@hotmail.com>
2020-02-10 01:25:41 -0300

    Aplicação pronta sem armazenamento de daddos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatDialogModule} from '@angular/material/dialog';
 
+/* Componentes criados para montar nossa página inicial */
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
+import { AppRoutingModule } from './app-routing.module';
+import { CadastroClientesComponent } from './cadastro-clientes/cadastro-clientes.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    /* Componentes declarados para montar nossa página inicial */
+    HeaderComponent,
+    ContentComponent,
+    FooterComponent,
+    CadastroClientesComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatDialogModule
+    MatDialogModule,
+    AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
